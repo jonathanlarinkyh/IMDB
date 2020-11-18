0df7d0874e1fc61d9aec68ae243f41abc7d0cfbb
AmajWardak <56152274+amajwardak@users.noreply.github.com>
2020-11-18 15:12:34 +0100

    Here is my commit with first testsuite

---
+++ page.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+class PageObject:
+    def page_down(self):
+        self.driver.find_element_by_tag_name("html").send_keys(Keys.PAGE_DOWN)
+
+    def page_up(self):
+        self.driver.find_element_by_tag_name("html").send_keys(Keys.PAGE_UP)
+
+    def accept_cookies(self):
+        self.driver.find_element_by_id("cn-accept-cookie").click()
+
+
+class IMDBMainPage(PageObject):
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+        pass
---
+++ test_Imdb.py
@@ -0,0 +1,71 @@
+import page
+import time
+import unittest
+
+import HtmlTestRunner
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('ignore-certificate-errors')
+print(help(EC.presence_of_all_elements_located))
+
+
+def webdriver_chrome():
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    return driver
+
+
+def webdriver_chrome_headless():
+    options.headless = True
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.set_window_size(1920, 1080)
+    driver.implicitly_wait(10)
+    return driver
+
+
+def webdriver_factory():
+    return webdriver_chrome_headless()
+
+class IMDBTest(unittest.TestCase):
+    # declare variable to store the URL to be visited
+    base_url = "https://www.imdb.com"
+    time.sleep(2)
+
+    # --- Pre - Condition ---
+
+    def setUp(self):
+        # declare and initialize driver variable
+        self.driver = webdriver.Chrome()
+        # close the app dialogue page
+        self.driver.get("https://imdb.com")
+        time.sleep(5)
+
+    def tearDown(self):
+        self.driver.close()
+
+class test_IMBD_Nav(unittest.TestCase):
+    base_url = "https://www.hrbank.af"
+
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+        self.target_url = "https://www.imdb.com"
+
+    def setUp(self):
+        self.driver = webdriver_chrome()
+        self.driver.get("https://www.imdb.com")
+
+    def test_page_access(self):
+        main_page = test_IMBD_Nav(self.driver)
+        main_page.click_IMDBMainPage()
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name="IMDB_Tests",
+                                                           output='C:/Users/AmAj/Desktop/Python '
+                                                                  'Projects/IMDB/Jonathan/Reports'))
