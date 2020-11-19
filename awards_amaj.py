3df06aecd4adff982ff0e4fabae06db28b9edcaa
AmajWardak <56152274+AmajWardak@users.noreply.github.com>
2020-11-19 13:56:51 +0100

    Initial Testing for Awards & Events done successfully.

--- awards_amaj.py
+++ awards_amaj.py
@@ -48,7 +48,7 @@ class IMDBTest(unittest.TestCase):
         self.driver.close()
 
 
-class foo_test_IMBD_Nav(unittest.TestCase):
+class test_IMBD_Nav(unittest.TestCase):
     base_url = "https://www.imdb.com"
 
     def __init__(self, methodName: str = ...):
@@ -59,7 +59,7 @@ class foo_test_IMBD_Nav(unittest.TestCase):
         self.driver = webdriver_chrome()
         self.driver.get("https://www.imdb.com")
 
-    def foo_test_page_access(self):
+    def test_page_access(self):
         main_page = self.driver.get("https://imdb.com")
 
     def tearDown(self):
@@ -68,22 +68,140 @@ class foo_test_IMBD_Nav(unittest.TestCase):
 
 class test_imdb_menu(unittest.TestCase):
 
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+
     def setUp(self):
         self.driver = webdriver_chrome()
         self.driver.get("https://www.imdb.com")
-#        page.IMDBMainPage(self.driver).accept_cookies()
+
+    #        page.IMDBMainPage(self.driver).accept_cookies()
 
     def test_click_menu(self):
         main_page = page.IMDBMainPage(self.driver)
         main_page.click_menu_dd()
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Main Menu.png")
         time.sleep(5)
 
-    def test_menu_awards(self):
+    def test_001_menu_oscars(self):
         main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
         main_page.click_oscars()
-        self.assertEqual(self.driver.find_element_by_xpath("//*[@id='imdbHeader']/div[2]/aside/div/div[2]/div/div["
-                                                           "3]/span/label").text, "Awards & Events", msg="Wrong Page")
-        time.sleep(10)
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='OSCARS']").text, "OSCARS",
+                         msg="Wrong Page. NotOscars")
+
+        time.sleep(5)
+
+    def test_002_menu_BPW(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_best_picture_Winner()
+        time.sleep(5)
+
+    def test_003_menu_Golden_Globes(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Golden_Globes()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='GOLDEN GLOBES']").text,
+                         "GOLDEN GLOBES", msg="Not GOLDEN GLOBES")
+        #        self.driver.get_screenshot_as_file("Golden Globes" + ".png")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Golden Globe.png")
+        time.sleep(5)
+
+    def test_004_menu_Emmys(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Emmys()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='EMMYS']").text,
+                         "EMMYS", msg="Not Emmys")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Emmys.png")
+        time.sleep(5)
+
+    def test_005_menu_STARmeter_Awards(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_STARmeter_Awards()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='IMDb STARmeter AWARDS']").text,
+                         "IMDb STARmeter AWARDS", msg="Wrong Page Not STARmeter Awards")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Starmeter Award.png")
+        time.sleep(5)
+
+    def test_006_menu_SD_Comic_Con(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_SanDiego_Comic_Con()
+        self.assertEqual(self.driver.find_element_by_xpath("//*[@id='widget-nav']/div[1]/div/a/h1").text,
+                         "SAN DIEGO COMIC-CON", msg="Wrong Page Not SD COMIC CON")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/SD Comic Con.png")
+        time.sleep(5)
+
+    def test_007_menu_NY_Comic_Con(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_NY_Comic_Con()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='NEW YORK COMIC CON']").text,
+                         "NEW YORK COMIC CON", msg="Wrong Page Not NY COMIC CON")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/NY Comic Con.png")
+        time.sleep(5)
+
+    def test_008_menu_Sundance_FF(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Sundance_Film_Festival()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='SUNDANCE FILM FESTIVAL']").text,
+                         "SUNDANCE FILM FESTIVAL", msg="Wrong Page Not SUNDANCE FILM FESTIVAL")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Sundance FF.png")
+        time.sleep(5)
+
+    def test_009_menu_Toronto_Intl_FF(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Toronto_Intl_Film_Festival()
+        self.assertEqual(self.driver.find_element_by_xpath("//h1[normalize-space()='TORONTO INTERNATIONAL FILM FESTIVAL']").text,
+                         "TORONTO INTERNATIONAL FILM FESTIVAL", msg="Wrong Page Not TORONTO INTL FILM FESTIVAL")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Toronto Intl FF.png")
+        time.sleep(5)
+
+    def test_009_menu_AWARD_CENTRAL(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Awards_central()
+        self.assertEqual(
+            self.driver.find_element_by_xpath("//h1[normalize-space()='AWARDS CENTRAL']").text,
+            "AWARDS CENTRAL", msg="Wrong Page Not AWARD CENTRAL")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Award Central.png")
+        time.sleep(5)
+
+    def test_010_menu_Festival_Central(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_Festival_Central()
+        self.assertEqual(
+            self.driver.find_element_by_xpath("//h1[normalize-space()='FESTIVAL CENTRAL']").text,
+            "FESTIVAL CENTRAL", msg="Wrong Page Not FESTIVAL CENTRAL")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/Festival Central.png")
+        time.sleep(5)
+
+    def test_011_menu_All_Events(self):
+        main_page = page.IMDB_menu_awards(self.driver)
+        main_page.click_menu_dd()
+        time.sleep(2)
+        main_page.click_All_Events()
+        self.assertEqual(
+            self.driver.find_element_by_xpath("//h1[normalize-space()='All Events']").text,
+            "All Events", msg="Wrong Page Not All Events")
+        self.driver.save_screenshot("C:/Users/AmAj/Desktop/Python Projects/IMDB/Jonathan/SC_Amaj/All Events.png")
+        time.sleep(5)
 
     def tearDown(self):
         self.driver.close()
--- page.py
+++ page.py
@@ -2,6 +2,7 @@ from lib2to3.pgen2 import driver
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 
 class PageObject:
@@ -31,8 +32,57 @@ class IMDB_menu_awards(PageObject):
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
 
+    def click_menu_dd(self):
+        self.driver.find_element_by_xpath("//label[@id='imdbHeader-navDrawerOpen--desktop']").click()
+
     def click_oscars(self):
-        self.driver.find_element_by_link_text("Oscars").click()
+        self.driver.find_element_by_xpath("//a[@href='/oscars/?ref_=nv_ev_acd']").click()
+
+    def click_best_picture_Winner(self):
+        self.driver.find_element_by_xpath("//*[@id='imdbHeader']/div[2]/aside/div/div[2]/div/div["
+                                          "3]/span/div/div/ul/a[3]").click()
+
+    def click_Golden_Globes(self):
+        self.driver.find_element_by_xpath("//a[@href='/golden-globes/?ref_=nv_ev_gg']").click()
+    #
+
+    def click_Emmys(self):
+        self.driver.find_element_by_xpath("//a[@href='/emmys/?ref_=nv_ev_rte']").click()
+    #
+
+    def click_STARmeter_Awards(self):
+        self.driver.find_element_by_xpath("//a[@href='/starmeterawards/?ref_=nv_ev_sma']").click()
+    #
+
+    def click_SanDiego_Comic_Con(self):
+        self.driver.find_element_by_xpath("//a[@href='/comic-con/?ref_=nv_ev_comic']").click()
+    #
+
+    def click_NY_Comic_Con(self):
+        self.driver.find_element_by_xpath("//a[@href='/nycc/?ref_=nv_ev_nycc']").click()
+    #
+
+    def click_Sundance_Film_Festival(self):
+        self.driver.find_element_by_xpath("//a[@href='/sundance/?ref_=nv_ev_sun']").click()
+    #
+
+    def click_Toronto_Intl_Film_Festival(self):
+        self.driver.find_element_by_xpath("//a[@href='/toronto/?ref_=nv_ev_tor']").click()
+    #
+
+    def click_Awards_central(self):
+        self.driver.find_element_by_xpath("//a[@href='/awards-central/?ref_=nv_ev_awrd']").click()
+    #
+
+    def click_Festival_Central(self):
+        self.driver.find_element_by_xpath("//a[@href='/festival-central/?ref_=nv_ev_fc']").click()
+    #
+
+    def click_All_Events(self):
+        self.driver.find_element_by_xpath("//a[@href='https://www.imdb.com/event/all/?ref_=nv_ev_all']").click()
+
+
+
 
 
 class imdb(PageObject):
