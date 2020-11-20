67e6ea446fba154bb8cd566d022b26773242a49b
Jonathan <jonathan.larin@student.kyh.se>
2020-11-20 16:47:06 +0100

    Menu test, Page class

--- src/IMDB_Menu_Movies.py
+++ src/IMDB_Menu_Movies.py
@@ -31,49 +31,52 @@ class IMDBPageMenu(unittest.TestCase):
     def test_release_calendar_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_release_calendar()
 
     def test_dvd_blue_releases_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_dvd_blue_releases()
-        time.sleep(5)
 
     def test_top_rated_movies_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_top_rated_movies()
-        time.sleep(5)
 
     def test_most_popular_movies_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
+
         main_page.click_menu_dd_most_popular_movies()
-        time.sleep(5)
 
     def test_browse_movies_by_genre_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_browse_movies_by_genre()
-        time.sleep(5)
 
     def test_top_box_office_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_top_box_office()
-        time.sleep(5)
 
     def test_showtime_tickets_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_showtime_tickets()
         time.sleep(5)
 
     def test_in_theaters_in_dd(self):
         main_page = page.IMDBMenuMovies(self.driver)
         main_page.click_menu_dd()
+        time.sleep(1)
         main_page.click_menu_dd_in_theater()
-        time.sleep(5)
 
     def tearDown(self):
         self.driver.close()
