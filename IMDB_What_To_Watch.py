132b001bbbc2340b0f926adbcee12f4efc5b56a8
Puckobello <victor.dellnas@student.kyh.se>
2020-12-01 13:15:53 +0100

    Update tests for all categories under "WATCH"

--- IMDB_What_To_Watch.py
+++ IMDB_What_To_Watch.py
@@ -20,7 +20,44 @@ class IMDBTestCase(unittest.TestCase):
         self.driver.find_element_by_xpath("//div[2]/span/div/div/ul/a").click()
         time.sleep(2)
         assert self.driver.find_element_by_css_selector(".ipc-title--page-title > .ipc-title__text").text == "What to Watch"
-        time.sleep(3)
+
+    def test_latest_trailers(self):
+        """ Testar titeln i 'Latest Trailers'. """
+        self.driver.get('https://www.imdb.com')
+        self.driver.find_element_by_xpath("//label[@id='imdbHeader-navDrawerOpen--desktop']").click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//div[2]/span/div/div/ul/a[2]").click()
+        time.sleep(2)
+        assert self.driver.find_element_by_css_selector(".ipc-title__text").text == "Movie & TV trailers"
+
+    def test_IMDB_Originals(self):
+        """ Testar titeln i 'IMDb Originals'. """
+        self.driver.get('https://www.imdb.com')
+        self.driver.find_element_by_xpath("//label[@id='imdbHeader-navDrawerOpen--desktop']").click()
+        time.sleep(2)
+        self.driver.find_element_by_link_text("IMDb Originals").click()
+        time.sleep(2)
+        assert self.driver.find_element_by_css_selector("a > h1").text == "IMDb ORIGINALS"
+
+    def test_IMDB_Picks(self):
+        """ Testar titeln i 'IMDb Picks'. """
+        self.driver.get('https://www.imdb.com')
+        self.driver.find_element_by_xpath("//label[@id='imdbHeader-navDrawerOpen--desktop']").click()
+        time.sleep(2)
+        self.driver.find_element_by_link_text("IMDb Picks").click()
+        time.sleep(2)
+        assert self.driver.find_element_by_xpath("//h1").text == "IMDb PICKS"
+
+    def test_IMDB_Podcasts(self):
+        """ Testar titeln i 'IMDb Podscasts'. """
+        self.driver.get('https://www.imdb.com')
+        self.driver.find_element_by_xpath("//label[@id='imdbHeader-navDrawerOpen--desktop']").click()
+        time.sleep(2)
+        self.driver.find_element_by_link_text("IMDb Podcasts").click()
+        time.sleep(2)
+        assert self.driver.find_element_by_xpath("//h1").text == "IMDb PODCASTS"
+        self.driver.back()
+        time.sleep(2)
 
 
 if __name__ == '__main__':
