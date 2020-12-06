22069c7987ce8b4f136607cc1ab8293853c966d4
nuricanpinar <74652027+nuricanpinar@users.noreply.github.com>
2020-12-06 18:38:01 +0100

    Add files via upload

--- page.py
+++ page.py
@@ -1,23 +1,77 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PageObject:
+    def __init__(self):
+        self.driver = webdriver
+
     def page_down(self):
         self.driver.find_element_by_tag_name("html").send_keys(Keys.PAGE_DOWN)
 
     def page_up(self):
         self.driver.find_element_by_tag_name("html").send_keys(Keys.PAGE_UP)
 
-    def accept_cookies(self):
-        self.driver.find_element_by_id("cn-accept-cookie").click()
+    # def accept_cookies(self):
+    #     self.driver.find_element_by_id("cn-accept-cookie").click()
+
+    def find_element_clickable_element_by_xpath(self, selector, wait=10):
+        return WebDriverWait(self.driver, wait).until(EC.element_to_be_clickable((By.XPATH, selector)))
+
+    def find_element_clickable_element_by_css_selector(self, selector, wait=10):
+        return WebDriverWait(self.driver, wait).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
+
+    def find_visible_element_by_xpath(self, selector, wait=10):
+        return WebDriverWait(self.driver, wait).until(EC.visibility_of_element_located((By.XPATH, selector)))
 
 
 class IMDBMainPage(PageObject):
+
+        def __init__(self, driver: webdriver.Firefox):
+            super().__init__()
+            self.driver = driver
+
+        def click_menu_dd(self):
+            self.find_element_clickable_element_by_xpath("//label[contains(.,'Menu')]", wait=10).click()
+
+
+class IMDB_menu_community(PageObject):
+
     def __init__(self, driver: webdriver.Chrome):
+        super().__init__()
         self.driver = driver
 
+    def click_menu_dd(self):
+        self.find_element_clickable_element_by_xpath("//label[contains(.,'Menu')]", wait=10).click()
+
+    def click_Help_center(self):
+        self.find_element_clickable_element_by_xpath("/html/body/div[1]/nav/div[2]/aside/div/div[2]/div/div[6]/span/div/div/ul/a[1]" , wait=10).click()
+
+    def click_contributor_zone(self):
+        self.find_element_clickable_element_by_xpath("/html/body/div[1]/nav/div[2]/aside/div/div[2]/div/div[6]/span/div/div/ul/a[2]" , wait=10).click()
+
+    def click_polls(self):
+        self.find_element_clickable_element_by_xpath("/html/body/div[1]/nav/div[2]/aside/div/div[2]/div/div[6]/span/div/div/ul/a[3]" , wait=10).click()
+
         pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_Imdb.py
+++ test_Imdb.py
@@ -1,37 +1,36 @@
-import page
 import time
 import unittest
-
-import HtmlTestRunner
+import page
+from page import IMDB_menu_community, IMDBMainPage
+from unittest import TestCase
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
+import HtmlTestRunner
 
 
-options = webdriver.ChromeOptions()
+options = webdriver.FirefoxOptions()
 options.add_argument('ignore-certificate-errors')
 print(help(EC.presence_of_all_elements_located))
 
 
-def webdriver_chrome():
-    driver = webdriver.Chrome(options=options)
+def webdriver_Firefox():
+    driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     driver.maximize_window()
     return driver
 
 
-def webdriver_chrome_headless():
+def webdriver_Firefox_headless():
     options.headless = True
-    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Firefox()
     driver.set_window_size(1920, 1080)
     driver.implicitly_wait(10)
     return driver
 
 
 def webdriver_factory():
-    return webdriver_chrome_headless()
+    return webdriver_Firefox_headless()
+
 
 class IMDBTest(unittest.TestCase):
     # declare variable to store the URL to be visited
@@ -42,7 +41,7 @@ class IMDBTest(unittest.TestCase):
 
     def setUp(self):
         # declare and initialize driver variable
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
         # close the app dialogue page
         self.driver.get("https://imdb.com")
         time.sleep(5)
@@ -50,22 +49,88 @@ class IMDBTest(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
+
 class test_IMBD_Nav(unittest.TestCase):
-    base_url = "https://www.hrbank.af"
+    base_url = "www.imdb.com"
 
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
         self.target_url = "https://www.imdb.com"
 
     def setUp(self):
-        self.driver = webdriver_chrome()
+        self.driver = webdriver_Firefox()
         self.driver.get("https://www.imdb.com")
 
     def test_page_access(self):
         main_page = test_IMBD_Nav(self.driver)
-        main_page.click_IMDBMainPage()
+        main_page.click_IMDB_Nav()
+
+
+class test_imdb_menu_community(unittest.TestCase):
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+
+    def setUp(self):
+        self.driver = webdriver_Firefox()
+        self.driver.get("https://www.imdb.com")
+
+    def est_click_menu(self):
+        main_page = page.IMDBMainPage(self.driver)
+        main_page.click_menu_dd()
+
+    def test_click_help_center(self):
+        main_page = page.IMDB_menu_community(self.driver)
+        main_page.click_Help_center()
+
+class test_imdb_menu_community(unittest.TestCase):
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+
+    def setUp(self):
+        self.driver = webdriver_Firefox()
+        self.driver.get("https://www.imdb.com")
+
+    def est_click_menu(self):
+        main_page = page.IMDBMainPage(self.driver)
+        main_page.click_menu_dd()
+
+    def test_click_contributor_zone(self):
+        main_page = page.IMDB_menu_community(self.driver)
+        main_page.click_contributor_zone()
+
+
+class test_imdb_menu_community(unittest.TestCase):
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+
+    def setUp(self):
+        self.driver = webdriver_Firefox()
+        self.driver.get("https://www.imdb.com")
+
+    def est_click_menu(self):
+        main_page = page.IMDBMainPage(self.driver)
+        main_page.click_menu_dd()
+
+    def test_click_polls(self):
+        main_page = page.IMDB_menu_community(self.driver)
+        main_page.click_polls()
+
+
+class test_imdb_dropdown(PageObject):
+    def __init__(self, driver: webdriver.Firefox):
+        self.driver = driver
+
+    def click_help_center(self):
+        self.driver.find_element_by_css_selector(".dropdown:nth-child(1) > .dropdown-button").click()
+
+    def click_contributor(self):
+        self.driver.find_element_by_css_selector(".dropdown:nth-child(2) > .dropdown-button").click()
+
+    def click_polls(self):
+        self.driver.find_element_by_css_selector(".dropdown:nth-child(3) > .dropdown-button").click()
+
+
+
 
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name="IMDB_Tests",
-                                                           output='C:/Users/AmAj/Desktop/Python '
-                                                                  'Projects/IMDB/Jonathan/Reports'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name="Nuri_Test_IMDB"))
